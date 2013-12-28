e2b2ea9008e055b86e18cf4f6d0c0277c212a75e
Justin Durack <justin@meetmikey.com>
2013-12-27 17:35:30 -0800

    separated out web stuff

--- lib/config.py
+++
@@ -1,7 +0,0 @@
-import json
-
-configFilename = 'config.json'
-configFile = open(configFilename, 'r')
-
-configDataString = configFile.read()
-config = json.loads(configDataString)
--- lib/constants.py
+++
@@ -1,7 +0,0 @@
-import json
-
-constantsFilename = 'constants.json'
-constantsFile = open(constantsFilename, 'r')
-
-constantsDataString = constantsFile.read()
-constants = json.loads(constantsDataString)
